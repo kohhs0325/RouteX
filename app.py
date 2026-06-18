d91f3eda73c6e29fb4d85c04c1412eca87610940
kohhs0325 <kohhs.0325@gmail.com>
2026-06-18 21:41:44 +0900

    app.py

--- app.py
+++ app.py
@@ -85,8 +85,15 @@ def estimate_time(distance):
 # =========================
 def use_odsay(start, end, waypoints):
 
+    print("===== ODsay 함수 진입 =====")
+    print("start =", start)
+    print("end =", end)
+    
     start_loc = search_place(start)
     end_loc = search_place(end)
+
+    print("start_loc =", start_loc)
+    print("end_loc =", end_loc)
 
     if not start_loc or not end_loc:
         return None
